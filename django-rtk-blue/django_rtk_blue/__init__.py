849bfc1231324691a83844dfcf641f4e8f40b0bd
Maarten Nieber <hallomaarten@yahoo.com>
2022-06-28 08:38:54 +0200

    Add django_rtk_blue_apps and django_rtk_green_apps

---
+++ django-rtk-blue/django_rtk_blue/__init__.py
@@ -0,0 +1,6 @@
+django_rtk_blue_apps = [
+    "django_rtk_blue",
+    "django_rtk_magic_link",
+    "django_rtk_password",
+    "django_rtk_upfront",
+]
---
+++ django-rtk-green/django_rtk_green/__init__.py
@@ -0,0 +1,6 @@
+django_rtk_green_apps = [
+    "django_rtk_green",
+    "django_rtk_later",
+    "django_rtk_magic_link",
+    "django_rtk_password",
+]
