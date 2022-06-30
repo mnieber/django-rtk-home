e2d329e04990f84a8ef064a14370281aecad3592
Maarten Nieber <hallomaarten@yahoo.com>
2022-06-30 16:20:17 +0200

    Fix: django-rtk-green uses django-rtk-later

--- django-rtk-green/django_rtk_green/mutations/__init__.py
+++ django-rtk-green/django_rtk_green/mutations/__init__.py
@@ -1,5 +1,7 @@
 import graphene
 from django_rtk.mutations import ObtainJSONWebToken
+from django_rtk_later.mutations import ActivateAccount  # noqa
+from django_rtk_later.mutations import RegisterAccount  # noqa
 from django_rtk_later.mutations import Mutation as RegisterMutation
 from django_rtk_magic_link.mutations import Mutation as MagicLinkMutation
 from django_rtk_magic_link.mutations import RequestMagicLink  # noqa
@@ -8,8 +10,6 @@ from django_rtk_password.mutations import ChangePassword  # noqa
 from django_rtk_password.mutations import RequestPasswordReset  # noqa
 from django_rtk_password.mutations import ResetPassword  # noqa
 from django_rtk_password.mutations import Mutation as PasswordMutation
-from django_rtk_upfront.mutations import ActivateAccount  # noqa
-from django_rtk_upfront.mutations import RegisterAccount  # noqa
 
 
 class Mutation(
