b285c3d2e0779811cb690f37615400d67f8594f8
Maarten Nieber <hallomaarten@yahoo.com>
2023-03-27 11:26:25 +0200

    Add django_rtk_green_models

--- django-rtk-green/django_rtk_green/__init__.py
+++ django-rtk-green/django_rtk_green/__init__.py
@@ -4,3 +4,9 @@ django_rtk_green_apps = [
     "django_rtk_magic_link",
     "django_rtk_password",
 ]
+
+django_rtk_green_models = [
+    "django_rtk_later.ActivationToken",
+    "django_rtk_magic_link.MagicLinkToken",
+    "django_rtk_password.PasswordResetToken",
+]
