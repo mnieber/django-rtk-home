f9185cb28336c82fe00e4b2d9fb2f5fa87b35006
Maarten Nieber <hallomaarten@yahoo.com>
2023-07-25 08:24:19 +0200

    Remove django_rtk_green_models variable

--- django-rtk-green/django_rtk_green/__init__.py
+++ django-rtk-green/django_rtk_green/__init__.py
@@ -4,9 +4,3 @@ django_rtk_green_apps = [
     "django_rtk_magic_link",
     "django_rtk_password",
 ]
-
-django_rtk_green_models = [
-    "django_rtk_later.ActivationToken",
-    "django_rtk_magic_link.MagicLinkToken",
-    "django_rtk_password.PasswordResetToken",
-]
